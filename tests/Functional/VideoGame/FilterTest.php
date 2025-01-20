17a92836c430860b5638ac5972b2afbacc287eac
Kerkar Kevin <kerkar.kevin@gmail.com>
2025-01-20 10:01:49 +0100

    correction du nombre de fixtures dans l'usecase

--- tests/Functional/VideoGame/FilterTest.php
+++ tests/Functional/VideoGame/FilterTest.php
@@ -53,7 +53,7 @@ final class FilterTest extends FunctionalTestCase
         return [
             'without tags' => [
                 $this->createQueryParams(1, []),
-                'Affiche 10 jeux vidéo de 1 à 10 sur les 50 jeux vidéo',
+                'Affiche 10 jeux vidéo de 1 à 10 sur les 500 jeux vidéo',
                 ['Jeu vidéo 0', 'Jeu vidéo 1', 'Jeu vidéo 2', 'Jeu vidéo 3', 'Jeu vidéo 4', 'Jeu vidéo 5', 'Jeu vidéo 6', 'Jeu vidéo 7', 'Jeu vidéo 8', 'Jeu vidéo 9']
             ],
 //            'page 2 without tags ' => [
